c422d2d843b60a282b4708ade021f18edbec9941
Collins Koech <collinskoechck34@gmail.com>
2020-10-15 13:55:25 +0300

    navbar component update

--- gikomba-store/src/App.js
+++ gikomba-store/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
+import Home from './pages/Home';
 
 function App() {
   return (
+  
    <Home/>
+
   );
 }
 
---
+++ gikomba-store/src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="title">
+                Gikosh.com
+            </div>
+            <div className="navigation">
+                <button>Home</button>
+                <button>Store</button>
+                <button>About Us</button>
+                <button>Contact us</button>
+            </div>
+        </div>
+    )
+}
